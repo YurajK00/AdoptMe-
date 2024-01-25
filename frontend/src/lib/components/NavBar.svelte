9360b93c8f425ff76a22dd6b090fe7480ffa7b4c
yxx10101 <xyu653@aucklanduni.ac.nz>
2024-01-26 12:12:50 +1300

    Main page by Echo 5.0

---
+++ frontend/src/lib/components/NavBar.svelte
@@ -0,0 +1,20 @@
+<script>
+    import "$lib/css/mainPage1.css";
+    import { page } from "$app/stores";
+  
+  
+    $: path = $page.url.pathname;
+    
+  </script>
+
+<nav>
+    <!-- The hyperlink of other page should be changed-->
+    <ul>
+      <li><a href="/" class:active={path === "/"}>Home</a></li>
+      <li><a href="/" class:active={path === "/"}>Articles</a></li>
+      <li><a href="/" class:active={path === "/"}>Founder Story</a></li>
+      <button id="login"><a href="/" class:active={path === "/"}>Log in!</a></button>
+    </ul>
+  </nav>
+
+  
--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,21 +1,14 @@
  <script>
   import "$lib/css/mainPage1.css";
   import { page } from "$app/stores";
+  import NavBar from "../lib/components/NavBar.svelte";
 
 
   $: path = $page.url.pathname;
   
 </script>
 
-<nav>
-  <!-- The hyperlink of other page should be changed-->
-  <ul>
-    <li><a href="/" class:active={path === "/"}>Home</a></li>
-    <li><a href="/" class:active={path === "/"}>Articles</a></li>
-    <li><a href="/" class:active={path === "/"}>Founder Story</a></li>
-    <button id="login"><a href="/" class:active={path === "/"}>Log in!</a></button>
-  </ul>
-</nav>
+<NavBar />
 
 <body>
 <div class="container">
