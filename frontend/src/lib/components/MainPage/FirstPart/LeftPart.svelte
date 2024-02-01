9aac800d65cb1dce08c8c2cbddb001634e722d66
yxx10101 <xyu653@aucklanduni.ac.nz>
2024-02-01 13:28:34 +1300

    main page by Echo 11.0

--- frontend/src/lib/components/MainPage/FirstPart/LeftPart.svelte
+++ frontend/src/lib/components/MainPage/FirstPart/LeftPart.svelte
@@ -1,14 +1,58 @@
-      
+<script>
+  import { onMount } from 'svelte';
+  let images = ["/src/lib/image/scroll-1.png", "/src/lib/image/scroll-2.png", "/src/lib/image/scroll-3.png", "/src/lib/image/scroll-4.png", "/src/lib/image/scroll-5.png"];
+  let currentImage = 0;
+  let interval;
+
+  function toggleImage() {
+    currentImage = (currentImage + 1) % images.length;
+  }
+
+  function nextImage() {
+    currentImage = (currentImage + 1) % images.length;
+  }
+
+  function prevImage() {
+    currentImage = (currentImage - 1 + images.length) % images.length;
+  }
+
+  function startAutoPlay() {
+    interval = setInterval(nextImage, 3000); // Switch picture interval, unit: milliseconds
+  }
+
+  function stopAutoPlay() {
+    clearInterval(interval);
+  }
+
+  function handleClickNext() {
+    stopAutoPlay(); 
+    nextImage(); 
+    startAutoPlay(); 
+  }
+
+  function handleClickLast() {
+    stopAutoPlay(); 
+    prevImage(); 
+    startAutoPlay(); 
+  }
+
+  onMount(() => {
+    startAutoPlay();
+  });
+</script>
+
+
 <!-- Slide show part + activity notices-->
+
    <div class="leftPart">
-        <div class="slideshow">
-          <div id = "images-container">
-            <img class="list" src="/src/lib/image/scroll-1.png" alt="animal1"  />
-            <img class="list" src="/src/lib/image/scroll-2.png" alt="animal2"  />
-            <img class="list" src="/src/lib/image/scroll-3.png" alt="animal3"  />
-            <img class="list" src="/src/lib/image/scroll-4.png" alt="animal4"  />
-            <img class="list" src="/src/lib/image/scroll-5.png" alt="animal5"  />
-          </div>
+        <div class="slideshow" >
+            <div id = "images-container" style={`transform: translateX(-${currentImage * 100}%)`}>
+              {#each images as img (img)}
+                <img src={img} alt="pic">
+              {/each}
+            </div>
+              <button id="last" on:click={handleClickLast}>◀</button>
+              <button id="next" on:click={handleClickNext}>▶</button>
         </div>
     
         <div class="activityBox">
@@ -22,3 +66,4 @@
         </div>
     </div>
   </div>
+  
