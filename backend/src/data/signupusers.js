002b257d5a52fd294d0d0c3be15a2f0e75ce2a62
ZedZed34 <haun070@aucklanduni.ac.nz>
2024-02-06 01:35:58 +1300

    Changes 4.5

--- backend/src/data/signupusers.js
+++ backend/src/data/signupusers.js
@@ -2,6 +2,7 @@ import { getDatabase } from "../db/database.js";
 import bcrypt from "bcryptjs";
 
 const saltRounds = 10; //make 10 saltrounds
+const saltrounds1 = 10;
 
 export async function signup(userData) {
   console.log("Function call to signup...");
@@ -10,7 +11,7 @@ export async function signup(userData) {
   const { username, email, password, confirmedPassword } = userData;
 
   // Check if passwords match
-  if (password != confirmedPassword) {
+  if (password !== confirmedPassword) {
     throw new Error("Passwords do not match");
   }
 
@@ -22,9 +23,10 @@ export async function signup(userData) {
 
   // Hash and salt the password
   const hashedPassword = await bcrypt.hash(password, saltRounds);
+  const hashedConfirmedPassword = await bcrypt.hash(confirmedPassword, saltrounds1);
   
   const sql = "INSERT INTO Users (username, email, password ,confirmedPassword) VALUES (?, ?, ?,?)";
-  const values = [username, email, hashedPassword, confirmedPassword];
+  const values = [username, email, hashedPassword, hashedConfirmedPassword];
 
   try {
     const result = await db.run(sql, values);
